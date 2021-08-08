8633ef881095d89416e50a412654f8b49adfd3bc
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-08-08 19:34:23 -0400

    added workout model

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workout");
+
+router.post("/api/workout/bulk");
+
+router.get("/api/workout");
+
